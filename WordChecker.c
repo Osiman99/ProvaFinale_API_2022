8f83dae9d667a44a946d0d6dbddd3456cf41e3b0
Osiman99 <davideosimo@gmail.com>
2022-08-12 20:06:10 +0200

    searchNode

--- WordChecker.c
+++ WordChecker.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define N 1024
+
+struct Node{
+    char str[N];
+    struct Node *right;
+    struct Node *left;
+    struct Node *p;
+};
+
+struct Node *searchNode(struct Node *root, char str[N]);
 
 int main() {
-    printf("Hello, World!\n");
+    int k;
+    scanf("%d", &k);
+
     return 0;
 }
+
+struct Node *searchNode(struct Node *root, char str[N]){
+    if (root == NULL || strcmp(str, root->str) == 0){
+        return root;
+    }
+    if (strcmp(str, root->str) < 0){
+        return searchNode(root->left, str);
+    }else{
+        return searchNode(root->right, str);
+    }
+}
+
